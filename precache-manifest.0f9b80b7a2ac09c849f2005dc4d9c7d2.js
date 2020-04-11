9f164e6a65fd5195825454cea31a38ef1889d6cb
jakecapra <jacob.capra@mymail.champlain.edu>
2020-04-11 19:20:43 -0400

    Updates

---
+++ precache-manifest.0f9b80b7a2ac09c849f2005dc4d9c7d2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1278a0f39aacdf68d43fd2b1cf667bc6",
+    "url": "/rlbingo/index.html"
+  },
+  {
+    "revision": "39a6f8a796e4f80c2a28",
+    "url": "/rlbingo/static/css/main.073f58b5.chunk.css"
+  },
+  {
+    "revision": "1ca5d7d1e54fd461610e",
+    "url": "/rlbingo/static/js/2.16ba9aec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/rlbingo/static/js/2.16ba9aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39a6f8a796e4f80c2a28",
+    "url": "/rlbingo/static/js/main.1a5c3690.chunk.js"
+  },
+  {
+    "revision": "667271193ac55103ba6b",
+    "url": "/rlbingo/static/js/runtime-main.19a45e87.js"
+  },
+  {
+    "revision": "d7df5243cea0abf8e4484b160a0bf5ad",
+    "url": "/rlbingo/static/media/bg.d7df5243.png"
+  },
+  {
+    "revision": "019e214bc9eb9e8b0cd29498f1d75e1a",
+    "url": "/rlbingo/static/media/dropshotbg.019e214b.mp4"
+  },
+  {
+    "revision": "b3b4ee1ca35da3774b975273856f62eb",
+    "url": "/rlbingo/static/media/githubLogo.b3b4ee1c.png"
+  },
+  {
+    "revision": "ae9d4fa4177a90cff65fa6d0740ed570",
+    "url": "/rlbingo/static/media/redditLogo.ae9d4fa4.png"
+  },
+  {
+    "revision": "aabc11272851376758535cc6acabc3d8",
+    "url": "/rlbingo/static/media/steamLogo.aabc1127.png"
+  },
+  {
+    "revision": "79b18605528b9418dd3cf4322d591f73",
+    "url": "/rlbingo/static/media/youTubeLogo.79b18605.png"
+  }
+]);
